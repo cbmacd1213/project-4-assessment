4c86f973ac6e385fce9c871519bf5cf8aee4a63e
Colin MacDonald <colinmacdonald1213@yahoo.com>
2020-10-16 16:46:55 -0400

    Updates

---
+++ precache-manifest.b168201b9416aae9ef72352115e43539.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05232aaece266735d20d30ea7999b34c",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "816e2c57e1f63b268fe0",
+    "url": "/project-4-assessment/static/css/main.908472bf.chunk.css"
+  },
+  {
+    "revision": "608118624ae7ee197d4b",
+    "url": "/project-4-assessment/static/js/2.e208ecfd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/project-4-assessment/static/js/2.e208ecfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "816e2c57e1f63b268fe0",
+    "url": "/project-4-assessment/static/js/main.074430ba.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
